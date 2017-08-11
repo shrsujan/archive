664c161500e632de0d9b41ccfc5851ed3bee71c5
Sujan Shrestha <shrsujan2007@gmail.com>
2017-08-11 15:07:19 +0545

    temporary fix signup form and its validation

--- app/mixins/validations/signup.js
+++ app/mixins/validations/signup.js
@@ -7,24 +7,35 @@ export default Ember.Mixin.create(EmberValidations, {
       presence: {
         message: 'Enter full name'
       },
+      format: {with: /^[a-z0-9A-Z.,! \s-]+$/, allowBlank: true, message: 'Sorry! Only alphanumeric values are allowed'},
       length: {
         minimum: 5,
-        maximum: 100
+        maximum: 100,
+        messages: {
+          tooShort: 'Your full name must be at least 5 characters',
+          tooLong: 'Your full name can not exceed 100 characters'
+        }
       }
     },
     'model.email': {
       presence: {
-        message: 'Enter email'
-      }
+        message: 'Enter email address'
+      },
+      format: {with: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,5}$/i,allowBlank: true, message: 'Please enter a valid email address. Eg: johndoe@gmail.com'}
     },
     'model.password': {
       presence: {
-        message: 'Enter password'
+        message: 'Enter your password'
+      },
+      length: { minimum: 6, allowBlank: true, messages: { tooShort: 'Your password must be at least 6 characters'} },
+      format: {with: /^[A-Za-z0-9_@./#&+-]*$/, allowBlank: true, message: 'These characters are not allowed'},
+      confirmation: {
+        message: 'Confirmation password does not match your match password'
       }
     },
-    'model.confirmPassword': {
+    'model.passwordConfirmation': {
       presence: {
-        message: 'Enter confirm password'
+        message: 'Confirm your password'
       }
     }
   }
--- app/signup/controller.js
+++ app/signup/controller.js
@@ -1,14 +1,16 @@
 import Ember from 'ember';
 import SignUpValidation from 'archive/mixins/validations/signup';
+const { get, set } = Ember;
 
 export default Ember.Controller.extend(SignUpValidation, {
+  hide: 'hide',
+  
   actions: {
     signup() {
-      this.validate().then(() => {
-        
-      }).catch((e) => {
-        console.error(e);
-      })
+      if (!get(this, 'isValid')) {
+        set(this, 'hide', '');
+        return;
+      }
     }
   }
 });
